f4402a60443a0fcad3399a1ee3a006bc4b9468f4
Amin397 <aminkhademi8@gmail.com>
2019-11-20 03:09:24 -0800

    access detailes for item click fixed

--- app/src/main/java/com/example/sportcup/activities/Item_detiels.java
+++ app/src/main/java/com/example/sportcup/activities/Item_detiels.java
@@ -35,7 +35,7 @@ public class Item_detiels extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "نقشه و موقعیت سالن ورزشی !", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
