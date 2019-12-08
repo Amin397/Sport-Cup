4ce8ac4c6f722c1684b701d32cee365982e55729
Amin397 <aminkhademi8@gmail.com>
2019-12-08 19:23:54 +0330

    Login page created

--- app/src/main/java/com/example/sportcup/splash_screen/SplashScreen.java
+++ app/src/main/java/com/example/sportcup/splash_screen/SplashScreen.java
@@ -1,29 +1,91 @@
 package com.example.sportcup.splash_screen;
 
+import android.animation.Animator;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.sportcup.activities.hoviat.Hoviat;
 import com.example.sportcup.R;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private Handler handler;
+    private TextView txt_splash;
+    private ProgressBar loadProgressBar;
+    private RelativeLayout rootView , afteranimation;
+    private LinearLayout logos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN , WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
-        handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        initView();
+
+        new CountDownTimer(5000, 1000) {
             @Override
-            public void run() {
-                startActivity(new Intent(SplashScreen.this , Hoviat.class));
-                finish();
+            public void onTick(long l) {
+
             }
-        } , 4000);
+
+            @Override
+            public void onFinish() {
+                logos.setVisibility(View.GONE);
+                loadProgressBar.setVisibility(View.GONE);
+                rootView.setBackgroundColor(ContextCompat.getColor(SplashScreen.this , R.color.colorSplashText));
+                txt_splash.setTextColor(Color.parseColor("#DFC42155"));
+                txt_splash.setTextSize(60f);
+                animation();
+            }
+        }.start();
+    }
+
+    private void initView() {
+        txt_splash = findViewById(R.id.txt_splashname_id);
+        loadProgressBar = findViewById(R.id.loadingProgressBar);
+        logos = findViewById(R.id.logos);
+        rootView = findViewById(R.id.rootview_id);
+        afteranimation = findViewById(R.id.afterAnimationView);
+    }
+
+    private void animation() {
+        ViewPropertyAnimator viewPropertyAnimator = txt_splash.animate();
+        viewPropertyAnimator.y(100f);
+        viewPropertyAnimator.setDuration(1000);
+        viewPropertyAnimator.setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                afteranimation.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
     }
 }
